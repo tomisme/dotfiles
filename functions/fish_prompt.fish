0b9546ae6fabe8427e51e8ef8833261654e7c6ed
Tom Hutchinson <git@tomisme.com>
2014-11-01 16:20:30 +0800

    update

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -3,13 +3,13 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
   end
 
-  printf '\n\u250C\u2500\u2524 \u25D4%s%s %s%s@%s %s%s %s%s \f\r\u2514\u2500\u25B6 ' \
+  printf '\n\u250C\u2500\u2500 %s%s %s%s@%s %s%s %s%s \f\r\u2514\u2500\u25B6 ' \
     (set_color yellow) \
     (date "+%H:%M") \
     (set_color blue) \
     $USER \
     $__fish_prompt_hostname \
-    (set_color red) \
+    (set_color $fish_color_cwd) \
     (prompt_pwd) \
     (set_color normal)
 end
