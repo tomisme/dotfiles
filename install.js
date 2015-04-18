e2fb0f90911158f03bb42577b689aace1c913515
Tom Hutchinson <git@tomisme.com>
2015-04-19 00:19:06 +0800

    update

--- install.js
+++ install.js
@@ -1,26 +1,28 @@
-var rimraf = require('rimraf');
-var fs = require('fs');
-var path = require('path');
+'use strict'
 
-var home = '/home/tom/';
+const rimraf = require('rimraf');
+const fs = require('fs');
+const path = require('path');
 
-var files = [
+const HOME = '/home/tom/';
+
+const files = [
+  { src: 'atom/keymap.cson', dest: '.atom/keymap.cson' },
+  { src: 'atom/styles.less', dest: '.atom/styles.less' },
   { src: 'fish/functions', dest: '.config/fish/functions' },
   { src: 'fish/config.fish', dest: '.config/fish/config.fish' },
-  { src: 'dunst/dunstrc', dest: '.config/dunst/dunstrc' },
   { src: 'git/gitconfig', dest: '.gitconfig' },
   { src: 'git/gitignore', dest: '.gitignore' },
+  { src: 'tmux/tmux.conf', dest: '.tmux.conf' },
   { src: 'vim', dest: '.vim' },
   { src: 'X/xbindkeysrc', dest: '.xbindkeysrc' },
   { src: 'X/xinitrc', dest: '.xinitrc' },
-  { src: 'tmux/tmux.conf', dest: '.tmux.conf' },
-  { src: 'dircolors/monokai', dest: '.dir_colors' },
   { src: 'fonts', dest: '.fonts' }
 ];
 
 files.map(function(file) {
-  var dest = path.join(home, file.dest);
-  var src = path.join(__dirname, file.src);
+  let dest = path.join(HOME, file.dest);
+  let src = path.join(__dirname, file.src);
 
   rimraf(dest, function() {
     console.log('Removed ' + dest);
