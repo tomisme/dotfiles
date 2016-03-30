d6992c84434c66809f2b610f7d22d661ca29acdb
Tom Hutchinson <git@tomisme.com>
2016-03-30 15:52:11 +0800

    atom updates

--- install.js
+++ install.js
@@ -7,14 +7,21 @@ const path = require('path');
 const HOME = '/home/tom/';
 
 const files = [
+  { src: 'fish/config.fish',   dest: '.config/fish/config.fish' },
+
   { src: 'atom/snippets.cson', dest: '.atom/snippets.cson' },
   { src: 'atom/keymap.cson',   dest: '.atom/keymap.cson' },
   { src: 'atom/config.cson',   dest: '.atom/config.cson' },
-  { src: 'fish/config.fish',   dest: '.config/fish/config.fish' },
+
+  { src: 'lein/profiles.clj',  dest: '.lein/profiles.clj' },
+
   { src: 'git/gitconfig',      dest: '.gitconfig' },
   { src: 'git/gitignore',      dest: '.gitignore' },
+
   { src: 'X/xinitrc',          dest: '.xinitrc' },
+
   { src: 'npm/npmrc',          dest: '.npmrc' },
+
   { src: 'vim',                dest: '.vim' }
 ];
 
