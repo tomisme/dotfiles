2b062f414c4e5c90e8031debc4f299a5502b8898
Tom Hutchinson <git@tomisme.com>
2017-06-06 19:37:15 +0800

    Hey!

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-bikeshed "0.3.0"]
                   [lein-kibit "0.1.2"]
-                  [nightlight/lein-nightlight "1.3.0"]]}}
+                  [cider/cider-nrepl "0.13.0"]
+                  [nightlight/lein-nightlight "1.6.5"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
